20b949fc29d21b3367698b0dc2d794d873272c38
prade <prade@DESKTOP-CGCOH45.mshome.net>
2018-06-03 12:28:22 +0530

    Home Page with links to Add and Read Notes pages

---
+++ jotdown/src/main/java/com/pradeep/jotdown/JotdownApplication.java
@@ -0,0 +1,12 @@
+package com.pradeep.jotdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JotdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JotdownApplication.class, args);
+	}
+}
